669ed607e4e203fa76959683144b8b3bc33a22d1
tzone85 <tzone85@gmail.com>
2019-08-05 19:29:13 +0200

    added navbar component and the navigation to all the existing components thus far

--- src/App.js
+++ src/App.js
@@ -5,12 +5,14 @@ import Home from './pages/Home';
 import Rooms from './pages/Rooms';
 import Error from './pages/Error';
 import SingleRoom from './pages/SingleRoom';
+import Navbar from './components/Navbar';
 
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 function App() {
   return (
     <Router>
+        <Navbar/>
         <Switch>
             <Route exact path="/" exact component={Home} />
             <Route exact path="/rooms/" component={Rooms} />
